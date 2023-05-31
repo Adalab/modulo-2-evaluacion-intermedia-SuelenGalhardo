87c7af9fcea6ff2b0ccf0bea8c46642a519165aa
SuelenGalhardo <suelenranucci@gmail.com>
2023-05-31 10:32:27 +0200

    add function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const select = document.querySelector('.js__select');
-const input = document.querySelector('.js__input');
+const inputApuesta = document.querySelector('.js__input__apuesta');
 const btn = document.querySelector('.js__btn');
 const saldo = document.querySelector('.js__saldo');
 const result = document.querySelector('.js__result');
@@ -13,10 +13,13 @@ function getRandomNumber(max) {
 btn.addEventListener('click', () => {
   const selectValue = select.value;
   const randomNumber = getRandomNumber(6);
-  console.log(selectValue, randomNumber);
+
   if (selectValue == randomNumber) {
+    const nuevoSaldo = inputApuesta.value * 2 + saldo.value;
+    //saldo.innerHTML = nuevoSaldo;
+    console.log('test', inputApuesta.value * 2, saldo.innerHTML);
     result.innerHTML = 'Has ganado el doble de lo apostado';
   } else {
-    result.innerHTML = 'Has perdido lo apostado';
+    result.innerHTML = 'Has perdido lo apostado, el numero generado fue: ' + randomNumber;
   }
 });
