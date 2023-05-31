d1b4a5badcb2c9d20de6e0499039afb176f57194
SuelenGalhardo <suelenranucci@gmail.com>
2023-05-31 10:03:02 +0200

    add function

--- main.js
+++ main.js
@@ -4,9 +4,19 @@ const select = document.querySelector('.js__select');
 const input = document.querySelector('.js__input');
 const btn = document.querySelector('.js__btn');
 const saldo = document.querySelector('.js__saldo');
+const result = document.querySelector('.js__result');
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-btn.addEventListener('click', () => {});
+btn.addEventListener('click', () => {
+  const selectValue = select.value;
+  const randomNumber = getRandomNumber(6);
+  console.log(selectValue, randomNumber);
+  if (selectValue == randomNumber) {
+    result.innerHTML = 'Has ganado el doble de lo apostado';
+  } else {
+    result.innerHTML = 'Has perdido lo apostado';
+  }
+});
