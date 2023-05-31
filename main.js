5a983fee3b901f745e956fb7d719f98541af807b
SuelenGalhardo <suelenranucci@gmail.com>
2023-05-31 09:17:21 +0200

    maquetacion y const

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const select = document.querySelector('.js__select');
+const input = document.querySelector('.js__input');
+const btn = document.querySelector('.js__btn');
+const saldo = document.querySelector('.js__saldo');
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+btn.addEventListener('click', () => {});
